1be6184f49a548719d0a183aa93569f3df7ee859
Benjamin Tan <tanbamboo@gmail.com>
2016-08-08 14:44:39 +0800

    Initial project with vue-cli

---
+++ src/components/Chart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    this is a chart component written by vue.
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data () {
+      return {
+        type: 'bar'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
